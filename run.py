7934c2d92bec5a2ed338906d28e7328f9db633f2
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2016-11-03 00:24:19 +0000

    Drawing a rectangle around the currently selected cell
    + slight changes (using a pyglet primitive for the cursor)

--- run.py
+++ run.py
@@ -9,21 +9,37 @@ class WorldView(object):
                      Cell.WATER: 'water.png', Cell.MOUNTAIN: 'mountain.png'}
     CELL_SIZE = 32
 
-    def __init__(self, world):
+    def __init__(self, window, world):
+        self.window = window
         self.world = world
         self.textures = {cell_type: pyglet.resource.image(
             'res/img/tiles/{}'.format(self.CELL_TEXTURES[cell_type])) for cell_type in list(Cell)}
+        self.cursor_position = None
+        self.selected_cell = None
+
+    def set_cursor_position(self, x, y):
+        self.cursor_position = (x, y)
+        self.selected_cell = (x // self.CELL_SIZE, y // self.CELL_SIZE)
 
     def draw(self):
+        # Drawing all cells
         for x in range(self.world.width):
             for y in range(self.world.height):
                 self.textures[self.world.cells[y][x]].blit(
                     x * self.CELL_SIZE, y * self.CELL_SIZE)
 
+        # Drawing rectangle around the currently selected cell
+        if self.selected_cell is not None:
+            x, y = self.selected_cell[0] * self.CELL_SIZE, self.selected_cell[1] * self.CELL_SIZE
+            dx = dy = self.CELL_SIZE
+            vertices = [x, y, x + dx, y, x + dx, y + dy, x, y + dy]
+            pyglet.graphics.draw(
+                4, pyglet.gl.GL_LINE_LOOP, ('v2f', vertices), ('s3B', (0, 0, 255, 0, 255, 0, 255, 0, 0, 255, 255, 255)))
+
 
 class MainWindow(pyglet.window.Window):
     DEFAULT_WORLD_SIZE = 35
-    BASE_DISPLACEMENT = 15
+    BASE_DISPLACEMENT = 25
     MOTION_DISPLACEMENTS = {pyglet.window.key.MOTION_RIGHT: (+BASE_DISPLACEMENT, 0), pyglet.window.key.MOTION_LEFT: (
         -BASE_DISPLACEMENT, 0), pyglet.window.key.MOTION_UP: (0, +BASE_DISPLACEMENT), pyglet.window.key.MOTION_DOWN: (0, -BASE_DISPLACEMENT)}
 
@@ -35,11 +51,13 @@ class MainWindow(pyglet.window.Window):
 
         # World/map management
         self.world = World(width=self.DEFAULT_WORLD_SIZE, height=self.DEFAULT_WORLD_SIZE)
-        self.world_view = WorldView(self.world)
+        self.world_view = WorldView(self, self.world)
 
         # Various graphical details
         sword_image = pyglet.resource.image('res/img/shortsword.png')
-        self.sword_sprite = pyglet.sprite.Sprite(sword_image, x=50, y=100)
+        sword_cursor = pyglet.window.ImageMouseCursor(
+            sword_image, sword_image.width, sword_image.height)
+        self.set_mouse_cursor(sword_cursor)
         self.label = pyglet.text.Label('Debug',
                                        color=(255, 255, 255, 125),
                                        font_size=12,
@@ -51,7 +69,6 @@ class MainWindow(pyglet.window.Window):
         Viewport.set_camera(self.x, self.y)
         with self.viewport:
             self.world_view.draw()
-        self.sword_sprite.draw()
         self.label.draw()
         self.fps_display.draw()
 
@@ -68,8 +85,7 @@ class MainWindow(pyglet.window.Window):
         print('Mouse pressed on coordinates ({}, {})'.format(x, y))
 
     def on_mouse_motion(self, x, y, dx, dy):
-        self.sword_sprite.x = x
-        self.sword_sprite.y = y
+        self.world_view.set_cursor_position(self.x + x, self.y + y)
 
 
 if __name__ == '__main__':
