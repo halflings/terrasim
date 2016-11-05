b46acbaf61876245939089db854fce414adb3938
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2016-11-05 21:19:06 +0000

    Using batching and only drawing visible cells.
    Much faster rendering this way.

--- views.py
+++ views.py
@@ -1,3 +1,5 @@
+import math
+
 import pyglet
 
 from world import Cell
@@ -16,16 +18,32 @@ class WorldView(object):
         self.cursor_position = None
         self.selected_cell = None
 
+        self.batch = pyglet.graphics.Batch()
+        self.map_sprites = []
+
     def set_cursor_position(self, x, y):
         self.cursor_position = (x, y)
         self.selected_cell = (x // self.CELL_SIZE, y // self.CELL_SIZE)
 
+    def generate_sprites(self):
+        min_x = max(0, self.window.x // self.CELL_SIZE)
+        min_y = max(0, self.window.y // self.CELL_SIZE)
+        cell_width = (self.window.width // self.CELL_SIZE) + 1
+        cell_height = (self.window.height // self.CELL_SIZE) + 1
+        max_x = min(min_x + cell_width, self.world.width - 1)
+        max_y = min(min_y + cell_height, self.world.height - 1)
+
+        map_sprites = []
+        for x in range(min_x, max_x + 1):
+            for y in range(min_y, max_y + 1):
+                sprite = pyglet.sprite.Sprite(
+                    self.textures[self.world.cells[y][x]], x=x * self.CELL_SIZE, y=y * self.CELL_SIZE, batch=self.batch)
+                map_sprites.append(sprite)
+        return map_sprites
+
     def draw(self):
-        # Drawing all cells
-        for x in range(self.world.width):
-            for y in range(self.world.height):
-                self.textures[self.world.cells[y][x]].blit(
-                    x * self.CELL_SIZE, y * self.CELL_SIZE)
+        self.map_sprites = self.generate_sprites()
+        self.batch.draw()
 
         # Drawing rectangle around the currently selected cell
         if self.selected_cell is not None:
